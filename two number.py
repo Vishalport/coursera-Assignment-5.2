51429d4469d11062441cab5de7350d3fd7893daf
Er.vishal <68020188+come-vishal@users.noreply.github.com>
2020-12-20 18:23:51 +0530

    Add files via upload

---
+++ two number.py
@@ -0,0 +1,10 @@
+# Find sum of two integer numbers
+int1 = input('Enter first integer: ')
+int2 = input('Enter second integer: ')
+
+# Add two integer numbers
+sum = int(int1) + int(int2)
+
+# Show the sum
+print('The sum of {0} and {1} is {2}'.format(int1, int2, sum))
+
